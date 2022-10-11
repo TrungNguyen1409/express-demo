34a7635403d963677fb7de63c422ee26f946d343
TrungNguyen1409 <trungnguyenb0k30@gmail.com>
2022-10-11 15:09:04 +0200

    create a post request

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 
 const courses =  [
     { id: 1, name:"course1"},
@@ -16,13 +18,22 @@ app.get('/api/courses', (req,res)=>{
     res.send(courses);
 });
 
-// /api/courses/1
+app.post('/api/courses', (req,res)=> {
+
+    const course = {
+        id: courses.length+1,
+        name: req.body.name
+    };
+
+    courses.push(course);
+    res.send(course);
+});
 
 app.get('/api/courses/:id',(req,res)=>{
     const course = courses.find(c => c.id === parseInt(req.params.id));
     if(!course) res.status(404).send('The course with the given ID was not found')
     res.send(course); // 404
-})
+});
 
 
 
