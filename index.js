e0feedbb4c36ff9b859c5dd65abf63902b94796f
TrungNguyen1409 <trungnguyenb0k30@gmail.com>
2022-10-11 15:00:06 +0200

    create course data

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 const express = require('express');
 const app = express();
 
+
+const courses =  [
+    { id: 1, name:"course1"},
+    { id: 2, name:"course2"},
+    { id: 3, name:"course3"}
+]
 app.get('/', (req, res) => {
     res.send('Hello World!!!');
 });
 
 
 app.get('/api/courses', (req,res)=>{
-    res.send([1,2,3]);
+    res.send(courses);
 });
 
 // /api/courses/1
 
 app.get('/api/courses/:id',(req,res)=>{
-    res.send(req.params.id);
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) res.status(404).send('The course with the given ID was not found')
+    res.send(course); // 404
 })
 
-app.get('/api/posts/:year/:month',(req,res)=>{
-    res.send(req.params);
-})
+
 
 // PORTs
 const port = process.env.PORT || 3000;
