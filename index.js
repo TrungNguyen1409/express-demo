d85a67b0a48ee25385ce07f4c50c7d2dc1954d1a
TrungNguyen1409 <trungnguyenb0k30@gmail.com>
2022-10-11 14:33:59 +0200

    changes

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!!!');
+});
+
+
+app.get('/api/courses', (req,res)=>{
+    res.send([1,2,3]);
+});
+
+// /api/courses/1
+
+app.get('/api/courses/:id',(req,res)=>{
+    res.send(req.params.id);
+})
+
+
+// PORTs
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port} ...`))
